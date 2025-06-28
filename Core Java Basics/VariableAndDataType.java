c5cb308773528bb8179bb86433c5d624aa8eb574
surajsahani9321 <radhashyam9321@gmail.com>
2025-06-28 12:13:38 +0530

    variable and datatype program

---
+++ Core Java Basics/VariableAndDataType.java
@@ -0,0 +1,24 @@
+class VariableAndDataType{
+
+    public static void main(String[] s){
+
+        // Variable :- 
+        // (1) It is like a container in which we can store some value
+        // (2) It is the name of the memory location
+
+        // Data Type :- It define the type of the value which are going to be stored in the
+        // variable.
+
+        int a=5; // 101 a = 5
+        float f=6.6f; // 102 f = 6.6
+        char c='h';
+        String ss="Hello";
+        boolean b=true;
+
+        System.out.println("a :- "+a);
+        System.out.println("f :- "+f);
+        System.out.println("c :- "+c);
+        System.out.println("ss :- "+ss);
+        System.out.println("b :- "+b);
+    }
+}
