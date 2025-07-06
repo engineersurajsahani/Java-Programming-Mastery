f95026ead30be913acc3a5ce0d4c7f33e22bb387
surajsahani9321 <radhashyam9321@gmail.com>
2025-07-06 11:08:03 +0530

    conditional statements

---
+++ Core Java Basics/Conditional Statements/ConditionalStatements.java
@@ -0,0 +1,39 @@
+public class ConditionalStatements{
+
+    public static void main(String[] s){
+
+        // Conditional Statements 
+
+        // if 
+        // if else 
+        // if else if 
+        // switch cases 
+
+        int day=33;
+
+        if(day==1){
+            System.out.println("Monday");
+        }
+        else if(day==2){
+            System.out.println("Tuesday");
+        }
+        else if(day==3){
+            System.out.println("Wednesday");
+        }
+        else if(day==4){
+            System.out.println("Thursday");
+        }
+        else if(day==5){
+            System.out.println("Friday");
+        }
+        else if(day==6){
+            System.out.println("Saturday");
+        }
+        else if(day==7){
+            System.out.println("Sunday");
+        }
+        else{
+            System.out.println("Not Know");
+        }
+    }
+}
